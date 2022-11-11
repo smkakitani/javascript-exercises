31eada36437f6adcbb86906e85c74f8fe10f89a8
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-11-10 21:41:56 -0300

    Do some exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repString = "";
+    
+    if (num < 0) {
+        repString = "ERROR";
+    } else {
+        let i = 0;
+        while (i < num) {
+            repString = repString + string;
+            i++;
+        }
+    }
+
+    return repString;
 
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let arrText = text.split('');
+    let arrReverse = arrText.reverse();
+    let arrJoin = arrReverse.join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
