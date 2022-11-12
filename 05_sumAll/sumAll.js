bf4e92eeb2a24b08ae9a3fad18778f4421a3fb5f
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-11-12 19:53:52 -0300

    do exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    let total = 0;
+
+    if (typeof numStart !== 'number' || typeof numEnd !== 'number') {
+        return 'ERROR';
+    }
+
+    if (numStart < 0 || numEnd < 0) {
+        return 'ERROR';    
+    } 
+    
+    if (numStart < numEnd) {
+        for (let i = numStart; i <= numEnd; i++) {
+            total += i;
+        }
+        return total;
+    }
+
+    if (numStart > numEnd) {
+        for (let i = numEnd; i <= numStart; i++) {
+            total += i;
+        }
+        return total;
+    }
 
 };
 
