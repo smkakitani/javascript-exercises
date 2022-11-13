bbe5130447cf6f6696800b92554b8370f6830be4
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-11-12 21:17:39 -0300

    do exercises tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  let celsius = (x - 32) * (5/9);
+  let roundCelsius = Math.round(celsius * 10) / 10;
+  return roundCelsius;
 };
 
-const ctof = function() {
-
+const ctof = function(y) {
+  let fahrenheit = (y * (9/5) + 32);
+  let roundFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
