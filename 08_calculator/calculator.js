965b8730651384f0d4c2bba6d32e4ae1ee9da96f
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-12-05 19:20:05 -0300

    Resolve calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {  
+  return array.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+	if (n === 0) {
+    return 1;
+  }
+  for (let i = n; i >= 1; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
