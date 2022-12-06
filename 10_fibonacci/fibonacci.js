e9869721ede1ed2af5f330b3edba206dd94a6e5e
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-12-06 20:44:06 -0300

    Resolve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const x = parseInt(num);
+    if (x < 0) {
+    return "OOPS";
+    }
+    //fibonacci
+    const fibonacciNums = [1];
+    let numA = 0;
+    let numB = 1;
+    
+    for (let i = 0; i < x-1; i++) {
+        n = numA + numB;
+        numA = numB;
+        numB = n;
+        fibonacciNums.push(n);
+    }
+    //num position
+    let result = fibonacciNums[fibonacciNums.length - 1];
+    return result;
 };
 
 // Do not edit below this line
