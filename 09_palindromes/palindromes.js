fe485f7c43f52b1aa99e0ddb9f5b5016891675f5
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-12-06 17:52:45 -0300

    Resolve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arrayFromString = Array.from(string.toLowerCase()).filter(x => x >= "a" && x <= "z");
+    const stringLetters = arrayFromString.join("");
+    const reversedString = arrayFromString.reverse().join("");
+    
+    if (stringLetters === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
+    // console.log(stringLetters);
+    // console.log(reversedString);
 };
 
 // Do not edit below this line
