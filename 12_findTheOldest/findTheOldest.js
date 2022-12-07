5a069028bd3a2e7d16fb4d1a3eaff4e7175c2253
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-12-07 18:15:25 -0300

    Resolve exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();  
+    const olderPerson = people.reduce((oldest, person) => ((person.yearOfDeath || currentYear) - person.yearOfBirth) > ((oldest.yearOfDeath || currentYear) - oldest.yearOfBirth) ? person : oldest);
+    return olderPerson;
 };
 
 // Do not edit below this line
